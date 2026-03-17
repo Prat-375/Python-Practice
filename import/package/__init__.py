b3ca3f6d95e0163470bba56655f47bccd1d6a6ca
Prateek Kumar Nayak <prateek.nayak50@gmail.com>
2026-03-17 22:14:47 +0100

    import function and standard libraries usage practice

---
+++ import/package/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py is a special file in Python that is used to indicate that the directory it is present in is a Python package.
+#  It can be an empty file, but it can also contain initialization code for the package. 
+# When you import a package, the __init__.py file is executed, and any code within it will be run. 
+# This allows you to set up any necessary variables, functions, or classes that should be available when the package is imported.
+#  Additionally, __init__.py can be used to specify which modules should be imported when the package is imported, 
+# making it easier to manage the structure of your code.
